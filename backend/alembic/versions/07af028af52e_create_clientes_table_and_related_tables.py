ddcd0e1ee68cdf689ef3d46b8db4421914d00824
Enzo Almeida <enzo.alverde@gmail.com>
2025-08-11 20:58:43 -0300

    fix: Reorder migrations to create clientes table before foreign key constraint

--- backend/alembic/versions/07af028af52e_create_clientes_table_and_related_tables.py
+++ backend/alembic/versions/07af028af52e_create_clientes_table_and_related_tables.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '07af028af52e'
-down_revision: Union[str, Sequence[str], None] = '5ee30ffa9e84'
+down_revision: Union[str, Sequence[str], None] = '324418f810fa'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
